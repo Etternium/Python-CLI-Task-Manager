e0f0854d08a39ab3ecf341a70f134fc09a36ee55
Etternium <luka.mumelas@upcmail.ie>
2026-03-10 15:45:06 +0000

    implemented FastAPI logic

--- main.py
+++ main.py
@@ -1,100 +1,51 @@
 from services.task_service import add_task, mark_complete, delete_task, edit_task
 from storage.json_storage import save_tasks, load_tasks
+from fastapi import FastAPI, HTTPException
 
+app = FastAPI()
 tasks = load_tasks()
 
-def get_tasks():
-    if not tasks:
-        print("No tasks available")
-        return
-
-    for i, task in enumerate(tasks):
-        print(f"{i + 1}. {task['description']} [{task['complete']}]")
-
-def get_filtered_tasks(completed=None):
-    if not tasks:
-        print("No tasks available")
-        return
-
+@app.get("/tasks")
+def get_tasks(completed: bool | None = None):
+    
     if completed is None:
-        filtered_tasks = tasks
-    else:
-        filtered_tasks = [task for task in tasks if task["complete"] == completed]
+        return tasks
+    
+    return [task for task in tasks if task["complete"] == completed]
 
-    for i, task in enumerate(filtered_tasks):
-        print(f"{i + 1}. {task["description"]} [{task["complete"]}]")
+@app.get("/task/{index}")
+def get_task_by_index(index: int):
+    return tasks[index - 1]
 
-while(True):
-    print("--------------------------------------")
-    print("TASK MANAGER")
-    print("--------------------------------------")
-    print("1. Add Task")
-    print("2. Complete Task")
-    print("3. Delete Task")
-    print("4. List All Tasks")
-    print("5. List Completed Tasks")
-    print("6. List Pending Tasks")
-    print("7. Edit Task Description")
-    print("8. Exit")
-    print("--------------------------------------")
+@app.post("/tasks")
+def create_task(desc: str):
+    task = add_task(tasks, desc)
+    save_tasks(tasks)
+    return task
 
-    user_input = int(input("Choose an option: "))
-
-    if user_input == 1:
-        desc = input("Enter task description: ")
-        add_task(tasks, desc)
+@app.put("/tasks/{index}/complete")
+def complete_task(index: int):
+    try:
+        task = mark_complete(tasks, index - 1)
         save_tasks(tasks)
+        return task
+    except IndexError:
+        return HTTPException(status_code=404, detail="Task not found")
 
-    elif user_input == 2:
-        get_tasks()
-        index = int(input("Enter task index: ")) - 1
-
-        try:
-            mark_complete(tasks, index)
-            save_tasks(tasks)
-        except IndexError:
-            print("Invalid index")
-        except ValueError:
-            print("Please enter a valid index")
-
-    elif user_input == 3:
-        get_tasks()
-        index = int(input("Enter task index: ")) - 1
-
-        try:
-            delete_task(tasks, index)
-            save_tasks(tasks)
-        except IndexError:
-            print("Invalid index")
-        except ValueError:
-            print("Please enter a valid index")
-
-    elif user_input == 4:
-        get_filtered_tasks()
-
-    elif user_input == 5:
-        get_filtered_tasks(True)
-
-    elif user_input == 6:
-        get_filtered_tasks(False)
-
-    elif user_input == 7:
-        get_tasks()
-        index = int(input("Enter task index: ")) - 1
-
-        try:
-            new_desc = input("Enter new description: ")
-            edit_task(tasks, index, new_desc)
-            save_tasks(tasks)
-        except IndexError:
-            print("Invalid index")
-        except ValueError:
-            print("Please enter a valid index")
-
-    elif user_input == 8:
-        print("\nExiting program...")
+@app.put("/update")
+def update_task(index: int, desc: str):
+    try:
+        task = edit_task(tasks, index - 1, desc)
         save_tasks(tasks)
-        break
+        return task
+    except IndexError:
+        return HTTPException(status_code=404, detail="Task not found")
 
-    else:
-        print("Invalid input!!")
+@app.delete("/tasks/{index}")
+def remove_task(index: int):
+    try:
+        task = delete_task(tasks, index - 1)
+        save_tasks(tasks)
+        return {"deleted": task}
+    except IndexError:
+        return HTTPException(status_code=404, detail="Task not found")
--- services/task_service.py
+++ services/task_service.py
@@ -1,14 +1,19 @@
 def add_task(tasks, description):
-    tasks.append({
+    task = {
         "description": description,
         "complete": False
-    })
+    }
+
+    tasks.append(task)
+    return task
 
 def mark_complete(tasks, index):
     tasks[index]["complete"] = True
+    return tasks[index]
 
 def delete_task(tasks, index):
-    tasks.pop(index)
+    return tasks.pop(index)
 
-def edit_task(tasks, index, edited):
+def edit_task(tasks, index: int, edited: str):
     tasks[index]["description"] = edited
+    return tasks[index]
