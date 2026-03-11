284dbe5811e0e576acfbb29747b5c63c469b7035
Etternium <luka.mumelas@upcmail.ie>
2026-03-11 15:40:08 +0000

    removed redundant files

--- models/task.py
+++
@@ -1,8 +0,0 @@
-class Task:
-    def __init__(self, description, complete=False):
-        self.description = description
-        self.complete = complete
-        self.data ={
-            "description": self.description,
-            "complete": self.complete
-        }
